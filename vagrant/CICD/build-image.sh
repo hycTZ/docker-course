6cc9710762cf61fd85d34b954d26ea79ccaba5d3
liming6 <liming6@asiainfo.com>
2018-11-21 22:55:58 +0800

    脚本

---
+++ vagrant/CICD/build-image.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+MODULE=$1
+TIME=`date "+%Y%m%d%H%M"`
+GIT_REVISION=`git log -1 --pretty=format:"%h"`
+IMAGE_NAME=zhugeaming/${MODULE}:${TIME}_${GIT_REVISION}
+cd ${MODULE}
+docker build -t ${GIT_REVISION} .
+docker push ${GIT_REVISION}
---
+++ vagrant/CICD/deploy.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+IMAGE=`cat IMAGE_NAME`
+DEPLOYMENT=$1
+MODULE=$2
+echo "update image to ${IMAGE}"
+kubectl set image deployment/${DEPLOYMENT} ${MODULE} =${MODULE}
